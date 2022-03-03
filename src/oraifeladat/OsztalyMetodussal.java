e503e07a8f968e1f2f35daba293fec1df6107ec4
ForGetASD <vitayz60@gmail.com>
2022-03-03 15:34:14 +0100

    java fáradtvagyok

---
+++ src/oraifeladat/OsztalyMetodussal.java
@@ -0,0 +1,20 @@
+package oraifeladat;
+
+class MasikMetodus{
+    void masikMetodus(){
+        System.out.println("masik metodus");
+    }
+}
+
+public class OsztalyMetodussal {
+    public static void main(String[] args) {
+        //egyMetodus();
+        
+        OsztalyMetodussal om = new OsztalyMetodussal();
+        om.egyMetodus();
+    }
+
+    void egyMetodus() {
+        System.out.println("egy metodus");
+    }
+}
